63dbfc8ea7cced61249cc805e00e14ac4cf98a00
HumbertoCanales <19170146@uttcampus.edu.mx>
2022-03-01 12:35:21 -0600

    [UPD] Get functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const express = require('express');
+var cors = require('cors')
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./serviceAccount.json");
@@ -11,25 +12,62 @@ admin.initializeApp({
 
 const app = express()
 
+app.use(cors())
+
 const db = admin.firestore()
 
-app.get('/hello-world', (req, res) => {
+app.get('/', (req, res) => {
   return res.status(200).json({ message: 'Hello world'})
 })
 
-app.post('/api/movies', async (req, res) => {
-  await db.collection('movies').doc('/'+ req.body.id + '/').create({ name: req.body.name });
-  return res.json();
+// Movies
+app.get('/api/movies', async (req, res) => {
+  try{
+    const query = await db.collection('movies').get()
+    const docs = query.docs
+    const response = docs.map(doc => ({
+      id: doc.id,
+      name: doc.data().name,
+      description: doc.data().description
+    }))
+    return res.status(200).json(response)
+  }catch(e){
+    return res.status(500).send(e)
+  }
 })
 
 app.get('/api/movies/:id', async (req, res) => {
-  try {
+  try{
     const doc = db.collection('movies').doc(req.params.id)
     const item = await doc.get()
     const response = item.data()
     return res.status(200).json(response)
-  } catch (e) {
-    console.log(e)
+  }catch (e){
+    return res.status(500).send(e)
+  }
+})
+
+app.post('/api/movies', async (req, res) => {
+  try{
+    await db.collection('movies').add({ 
+      name: req.body.name, 
+      description: req.body.description 
+    })
+    return res.status(201).json()
+  }catch(e){
+    return res.status(500).send(e)
+  }
+})
+
+app.put('/api/movies/:id', async (req,res) => {
+  try {
+    const doc = db.collection('movies').doc(req.params.id)
+    await doc.update({ 
+      name: req.body.name, 
+      description: req.body.description 
+    })
+    return res.status(200).json()
+  } catch (e){
     return res.status(500).send(e)
   }
 })
@@ -38,35 +76,60 @@ app.delete('/api/movies/:id', async (req,res) => {
   try {
     const doc = db.collection('movies').doc(req.params.id)
     await doc.delete()
-    return res.json()
+    return res.status(200).json()
   } catch (e) {
     return res.status(500).send(e)
   }
 })
 
-app.put('/api/movies/:id', async (req,res) => {
-  try {
-    const doc = db.collection('movies').doc(req.params.id)
-    await doc.update({name: req.body.name})
-    return res.json()
-  } catch (e){
+// Categories
+app.get('/api/categories', async (req, res) => {
+  try{
+    const query = await db.collection('categories').get()
+    const docs = query.docs
+    const response = docs.map(doc => ({
+      id: doc.id,
+      name: doc.data().name,
+      description: doc.data().description
+    }))
+    return res.status(200).json(response)
+  }catch(e){
+    return res.status(500).send(e)
+  }
+})
+
+app.get('/api/categories/:id', async (req, res) => {
+  try{
+    const doc = db.collection('categories').doc(req.params.id)
+    const item = await doc.get()
+    const response = item.data()
+    return res.status(200).json(response)
+  }catch (e){
     return res.status(500).send(e)
   }
 })
 
 app.post('/api/categories', async (req, res) => {
-  await db.collection('categories').doc('/'+ req.body.id + '/').create({ name: req.body.name , description: req.body.description});
-  return res.json();
+  try{
+    await db.collection('categories').add({ 
+      name: req.body.name, 
+      description: req.body.description 
+    })
+    return res.status(201).json()
+  }catch(e){
+    return res.status(500).send(e)
+  }
 })
 
-app.get('/api/categories/:id', async (req, res) => {
+app.put('/api/categories/:id', async (req,res) => {
   try {
     const doc = db.collection('categories').doc(req.params.id)
-    const item = await doc.get()
-    const response = item.data()
-    return res.status(200).json(response)
-  } catch (e) {
-    console.log(e)
+    await doc.update({ 
+      name: req.body.name, 
+      description: req.body.description 
+    })
+    return res.status(200).json()
+  } catch (e){
     return res.status(500).send(e)
   }
 })
@@ -75,36 +138,60 @@ app.delete('/api/categories/:id', async (req,res) => {
   try {
     const doc = db.collection('categories').doc(req.params.id)
     await doc.delete()
-    return res.json()
+    return res.status(200).json()
   } catch (e) {
     return res.status(500).send(e)
   }
 })
 
-app.put('/api/categories/:id', async (req,res) => {
-  try {
-    const doc = db.collection('categories').doc(req.params.id)
-    await doc.update({name: req.body.name, description: req.body.description})
-    return res.json()
-  } catch (e){
+// Studios
+app.get('/api/studios', async (req, res) => {
+  try{
+    const query = await db.collection('studios').get()
+    const docs = query.docs
+    const response = docs.map(doc => ({
+      id: doc.id,
+      name: doc.data().name,
+      description: doc.data().description
+    }))
+    return res.status(200).json(response)
+  }catch(e){
     return res.status(500).send(e)
   }
 })
 
-
-app.post('/api/studios', async (req, res) => {
-  await db.collection('studios').doc('/'+ req.body.id + '/').create({ name: req.body.name , description: req.body.description});
-  return res.json();
-})
-
 app.get('/api/studios/:id', async (req, res) => {
-  try {
+  try{
     const doc = db.collection('studios').doc(req.params.id)
     const item = await doc.get()
     const response = item.data()
     return res.status(200).json(response)
-  } catch (e) {
-    console.log(e)
+  }catch (e){
+    return res.status(500).send(e)
+  }
+})
+
+app.post('/api/studios', async (req, res) => {
+  try{
+    await db.collection('studios').add({ 
+      name: req.body.name, 
+      description: req.body.description 
+    })
+    return res.status(201).json()
+  }catch(e){
+    return res.status(500).send(e)
+  }
+})
+
+app.put('/api/studios/:id', async (req,res) => {
+  try {
+    const doc = db.collection('studios').doc(req.params.id)
+    await doc.update({ 
+      name: req.body.name, 
+      description: req.body.description 
+    })
+    return res.status(200).json()
+  } catch (e){
     return res.status(500).send(e)
   }
 })
@@ -113,18 +200,8 @@ app.delete('/api/studios/:id', async (req,res) => {
   try {
     const doc = db.collection('studios').doc(req.params.id)
     await doc.delete()
-    return res.json()
+    return res.status(200).json()
   } catch (e) {
-    return res.status(500).send(e)
-  }
-})
-
-app.put('/api/studios/:id', async (req,res) => {
-  try {
-    const doc = db.collection('studios').doc(req.params.id)
-    await doc.update({name: req.body.name, description: req.body.description})
-    return res.json()
-  } catch (e){
     return res.status(500).send(e)
   }
 })
